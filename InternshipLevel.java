f2a1f95ddea6b28dbf3a16207aa59a073b0b2b91
Wen Jie <YEOW0088@e.ntu.edu.sg>
2025-11-12 00:21:15 +0800

    new

--- InternshipLevel.java
+++ InternshipLevel.java
@@ -1,5 +1,16 @@
 public enum InternshipLevel {
-    BASIC,
-    INTERMEDIATE,
-    ADVANCED
+    BASIC("Basic"),
+    INTERMEDIATE("Intermediate"),
+    ADVANCED("Advanced");
+
+    private final String displayName;
+
+    InternshipLevel(String displayName) {
+        this.displayName = displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
 }
