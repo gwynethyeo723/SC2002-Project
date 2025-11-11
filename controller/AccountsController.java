51299133ba7619bad794241a375ee3fd3823d121
Wen Jie <YEOW0088@e.ntu.edu.sg>
2025-11-12 03:46:36 +0800

    new

--- controller/AccountsController.java
+++ controller/AccountsController.java
@@ -2,23 +2,27 @@ package controller;
 
 import java.util.*;
 import entity.CareerCenterStaff;
+import entity.CompanyRep;
+import entity.User;
 import enumeration.CareerCenterStaffStatus;
+import enumeration.CompanyRepStatus;
 
 
 public class AccountsController {
     
     // Approve or reject a CompanyRep account
-    public void reviewCompanyRep(CareerCenterStaff staff, boolean approve) {
+    public static void reviewCompanyRep(CompanyRep rep,CareerCenterStaff staff, boolean approve) {
         if (!staff.isLoggedIn()) {
             System.out.println("You must be logged in to perform this action.");
             return;
         }
-        if(approve) {
-            staff.setStatus(CareerCenterStaffStatus.APPROVED);
-            System.out.println("CompanyRep " + staff.getName() + " approved.");
+
+        if (approve) {
+            rep.setStatus(CompanyRepStatus.APPROVED); // assuming you have a CompanyRepStatus enum
+            System.out.println("CompanyRep " + rep.getName() + " approved.");
         } else {
-            staff.setStatus(CareerCenterStaffStatus.REJECTED);
-            System.out.println("CompanyRep " + staff.getName() + " rejected.");
+            rep.setStatus(CompanyRepStatus.REJECTED);
+            System.out.println("CompanyRep " + rep.getName() + " rejected.");
         }
     }
 }
